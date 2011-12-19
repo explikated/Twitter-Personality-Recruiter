87013b5c87e435c091f0c220158d2a691f8e17b5
Kate Dramstad <calculated.mayhem@gmail.com>
2011-12-19 10:45:52 -0500

    change to pass users by id more of the time

--- script/search.rb
+++ script/search.rb
@@ -6,17 +6,18 @@ require './criteria'
 
 dict = {}
 
+statusf= File.open("status", "a+")
+
 #Read in username/status dictionary
 usersf= File.open("users", "a+").each { |line|
 	particip = line.split()
 
 	if dict.has_key?(particip[0]) == false
 		dict [particip[0]] = particip[1]
-	else
-		#ERROR something went wrong building the user dictionary
 	end
 	#User dictionary now includes all existing participants
 }
+userlookup = dict.invert
 
 id_counterf = File.open("id counter", "r")
 id_counters = id_counterf.readline
@@ -28,13 +29,14 @@ search = Twitter::Search.new
 search_terms = ["me"] 
 result = search.lang('en').geocode(41,-74,"250mi").containing(search_terms.join(" OR "))
 result.each do |tweet|
-	if dict.has_key?(tweet.from_user) == false
+	if userlookup.has_key?(tweet.from_user) == false
 		#Check activity restraints
 		stream = Twitter::Client.new
 		opt = {}
 		#Grab this many tweets from the user
 		opt["count"] = 1
 		res = stream.user_timeline(tweet.from_user, opt)
+		puts res.last.created_at
 		tmp = res.last.created_at.split()
 		m, d = tmp[1], tmp[2]
 		if criteria(m,d) == 1
@@ -47,8 +49,9 @@ result.each do |tweet|
 			streamf.close
 		end
 		#Add to dictionary
-		dict[tweet.from_user] = id_counter
-		usersf.write(tweet.from_user + " " + id_counter.to_s + "\n")
+		dict[id_counter] = tweet.from_user
+		usersf.write(id_counter.to_s + " " + tweet.from_user + "\n")
+		statusf.write(id_counter.to_s + " new\n")
 		id_counter += 1
 	else
 		#what to do if user exists
@@ -59,3 +62,4 @@ id_counterff = File.open("id counter", "w")
 id_counterff.write(id_counter.to_s)
 id_counterff.close
 usersf.close
+statusf.close
--- script/spam.rb
+++ script/spam.rb
@@ -2,26 +2,37 @@ require 'rubygems'
 require 'net/http'
 require 'net/https'
 require 'twitter'
-require './auth.rb'
+#require './auth.rb'
 require './env.rb'
 require './message.rb'
 
-dict = {}
+user_dict = {}
+status_dict = {}
+
 sends = []
 
 usersf = File.open("users", "a+").each do |line|
 	particip = line.split()
-	dict[particip.first] = particip.last
-	sends << particip.first if particip.last == "new"
+	user_dict[particip.first] = particip.last
 end
-usersf.truncate(0)
 
-sends.each do |user|
+statusf = File.open("status", "a+").each do |line|
+	idstatus = line.split()
+	status_dict[idstatus.first] = idstatus.last
+	sends << idstatus.first if idstatus.last == "new"
+end
+statusf.truncate(0)
+
+sends.each do |id|
+	user = user_dict[id]
 	Twitter.update(message(user)) if $ENV == 'production'
 	puts message(user) if $ENV == 'test'
-	dict[user] = "messaged-#{Time.now.to_s.split.join("/")}"
-	usersf.puts("#{user} #{dict[user]}")
+	status_dict[id] = "messaged-#{Time.now.to_s.split.join("/")}"
 	sleep (5 + rand(10))*60 if $ENV == 'production' #wait 5-15 minutes before sending next one
 end
 
+#write new status dict
+status_dict.each {|key, value| statusf.write("#{key} #{value}\n")}
+
+statusf.close
 usersf.close
